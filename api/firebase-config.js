524830bff6093b6c9e6f64c8c98b07e2a6a091fd
berekfros <elminato.kimanze@gmail.com>
2026-08-06 21:26:07 +0700

    Update firebase-config.js

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -3,11 +3,11 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "portal-koarmada-92e19.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID || "portal-koarmada-92e19",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "portal-koarmada-92e19.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "966060677941",
-  appId: process.env.FIREBASE_APP_ID || "1:966060677941:web:cd6074b5d8e2b3b9edca7c"
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
